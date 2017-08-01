28153454713003257ed7d03a5c1768bd002abd6e
C0618C <weicong75@gmail.com>
2017-08-01 15:33:47 +0800

    优化项目目录结构
    调整文件位置、调整打包方式

--- Release.bat
+++ Release.bat
@@ -1,14 +1,16 @@
 @echo off
 del /q Release\*
 
-for /f "tokens=* delims=" %%a in ('dir /b') do (
+for /r Web\ %%a in (*) do (
     echo %%a>tempv
-    (findstr "\.lua" tempv > nul && (
-        copy /y %%a Release\ > nul
-    ) || (echo  '') >nul )
     (findstr "\.css \.js \.html \.jpg \.png \.ico" tempv > nul && (
-        7z a -tgzip Release\%%a.gz %%a
+        7z a -tgzip %%a.gz %%a
     ) || (echo  '') >nul )
 )
+copy /y *.lua Release\
+move /y Web\*.gz Release\
+copy  /y LICENSE Release\
 
 del /q tempv
+
+tree Release /f >Release\__filelist__
